97181dfc3378a75384de99bd9df9f89c3c0f5fb6
HannibalKcc <zhuhuaruicn@163.com>
2017-07-11 09:41:19 +0800

    shopcart.vue购物车图标点亮 goods.vue两边的swiper联动

--- src/components/goods/panel/panel.vue
+++ src/components/goods/panel/panel.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="panel">
-    <div class="detailSub icon-remove_circle_outline" v-show="count" @click="subFood"></div>
-    <div class="detailCount" v-show="count">{{count}}</div>
+    <transition name="animSub">
+      <div class="detailSub icon-remove_circle_outline" v-show="details.count > 0 || count > 0" @click="subFood"></div>
+    </transition>
+    <div class="detailCount">
+      <span v-show="count > 0">{{count}}</span>
+      <span v-show="details.count > 0">{{details.count}}</span>
+    </div>
     <div class="detailAdd icon-add_circle" @click="addFood"></div>
   </div>
 </template>
@@ -9,6 +14,7 @@
 <script type="text/ecmascript-6">
   import Bus from '../../../common/js/bus.js';
   export default {
+//    没有设置接受函数
     props: {
       details: {}
     },
@@ -19,46 +25,71 @@
     },
     methods: {
       subFood: function () {
-        this.count--;
+        if (this.details.count > 0) {
+          this.details.count--;
+          this.count = this.details.count;
+          return;
+        }
+        if (this.count > 0) this.count--;
       },
       addFood: function () {
+        if (this.details.count > 0) {
+          this.details.count++;
+          this.count = this.details.count;
+          return;
+        }
         this.count++;
       }
     },
     watch: {
       count: function () {
-        Bus.$emit('update-shoplist', this.details.name, this.details.price, this.count);
+        let realCount = this.details.counts || this.count;
+        Bus.$emit('update-shoplist', this.details.name, this.details.price, realCount);
       }
     }
-  };
+  }
+  ;
 </script>
 
 <style scoped rel="stylesheet/less" type="text/less" lang="less">
+  /*动画效果*/
+  .animSub-enter-active, .animSub-leave-active {
+    transition: all .3s linear;
+  }
+
+  .animSub-enter, .animSub-leave-to {
+    transform: translateX(48px) rotate(180deg);
+    opacity: 0;
+  }
+
   .panel {
     /*display: inline-block;*/
     float: right;
+    vertical-align: middle;
     .detailSub {
       display: inline-block;
       vertical-align: middle;
-      line-height: 28px;
-      font-size: 28px;
-      color: blue;
+      line-height: 48px;
+      font-size: 48px;
+      color: rgb(0, 160, 220);
     }
     .detailCount {
       display: inline-block;
       width: 48px;
-      vertical-align: middle;
       text-align: center;
-      font-size: 20px;
-      line-height: 28px;
-      color: rgb(143, 157, 159);
+      span {
+        vertical-align: middle;
+        font-size: 20px;
+        line-height: 48px;
+        color: rgb(143, 157, 159);
+      }
     }
     .detailAdd {
       display: inline-block;
       vertical-align: middle;
-      line-height: 28px;
-      font-size: 28px;
-      color: blue;
+      line-height: 48px;
+      font-size: 48px;
+      color: rgb(0, 160, 220);
     }
   }
 </style>
